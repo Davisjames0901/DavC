f61f1f321b175ccb855b7331759d3e2a5575b3f0
Davisjames0901 <Diggly123456@gmail.com>
2016-12-18 00:48:41 -0500

    Revolution!

---
+++ Revolution/DavCRevolution/Interfaces/ILine/ILine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavCRevolution.Interfaces
+{
+    public interface ILine : ICodeObject
+    {
+        string GetText();
+    }
+}
---
+++ Revolution/DavCRevolution/lib/Interpreter.cs
@@ -0,0 +1,17 @@
+﻿using DavCRevolution.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavCRevolution.lib
+{
+    public class Interpreter
+    {
+        public string InterpretLine(ISourceFile context, ILine line)
+        {
+            return "";
+        }
+    }
+}
